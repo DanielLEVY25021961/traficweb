78fee989f40c4555287d3714b3c034d4b80fc9d1
DanielLEVY25021961 <dany.levy@free.fr>
2020-10-21 09:52:47 +0200

    MODIFICATION DE APPLICATION.java et APPLICATIONTest.java

--- sources/1_ms_configuration/src/main/java/levy/daniel/application/Application.java
+++ sources/1_ms_configuration/src/main/java/levy/daniel/application/Application.java
@@ -1,15 +1,68 @@
 package levy.daniel.application;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * CLASSE Application :<br/>
+ * Point d'entrée de l'application SPRING BOOT.<br/>
+ * L'annotation <code>SpringBootApplication</code> déclare à SPRING 
+ * qu'il s'agit de la classe APPLICATIVE.<br/>
+ * L'annotation <code>EnableConfigServer</code> déclare à SPRING 
+ * qu'il s'agit d'un MICRO-SERVICE TECHNIQUE de CENTRALISATION 
+ * de la CONFIGURATION.<br/>
+ * <br/>
+ *
+ * - Exemple d'utilisation :<br/>
+ *<br/>
+ * 
+ * - Mots-clé :<br/>
+ * <br/>
+ *
+ * - Dépendances :<br/>
+ * <br/>
+ *
+ *
+ * @author Daniel Lévy
+ * @version 1.0
+ * @since 20 oct. 2020
+ */
 @EnableConfigServer
 @SpringBootApplication
-public class Application {
+public class Application {  // NOPMD by dan on 20/10/2020 12:44
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+	// ************************ATTRIBUTS************************************/
 
-}
+	/**
+	 * LOG : Log : Logger pour Log4j (utilisant commons-logging).
+	 */
+	@SuppressWarnings("unused")
+	private static final Log LOG = LogFactory.getLog(Application.class);
+
+	// *************************METHODES************************************/
+
+	
+	
+	/**
+	 * CONSTRUCTEUR D'ARITE NULLE.
+	 */
+	public Application() {
+		super();
+	} // Fin du CONSTRUCTEUR D'ARITE NULLE.________________________________
+
+	
+	
+	/**
+	 * Point d'entrée de l'application.
+	 * @param pArgs : String[].
+	 */
+	public static void main(final String... pArgs) {
+		SpringApplication.run(Application.class, pArgs);
+	} // Fin de main(...)._________________________________________________
+
+	
+	
+} // FIN DE LA CLASSE Application.-------------------------------------------
--- sources/1_ms_configuration/src/test/java/levy/daniel/application/ApplicationTests.java
+++ sources/1_ms_configuration/src/test/java/levy/daniel/application/ApplicationTests.java
@@ -1,13 +1,61 @@
 package levy.daniel.application;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * CLASSE ApplicationTests :<br/>
+ * Classe de tests JUNIT 5 (Jupiter) fournie par SPRING.<br/>
+ * <br/>
+ *
+ * - Exemple d'utilisation :<br/>
+ *<br/>
+ * 
+ * - Mots-clé :<br/>
+ * <br/>
+ *
+ * - Dépendances :<br/>
+ * <br/>
+ *
+ *
+ * @author Daniel Lévy
+ * @version 1.0
+ * @since 20 oct. 2020
+ */
 @SpringBootTest
-class ApplicationTests {
+public class ApplicationTests {
 
+	// ************************ATTRIBUTS************************************/
+
+	/**
+	 * LOG : Log : Logger pour Log4j (utilisant commons-logging).
+	 */
+	@SuppressWarnings("unused")
+	private static final Log LOG = LogFactory.getLog(ApplicationTests.class);
+
+	// *************************METHODES************************************/
+
+	
+	
+	/**
+	 * CONSTRUCTEUR D'ARITE NULLE.
+	 */
+	public ApplicationTests() {
+		super();
+	} // Fin du CONSTRUCTEUR D'ARITE NULLE.________________________________
+	
+	
+
+	/**
+	 * 
+	 */
 	@Test
-	void contextLoads() {
-	}
+	public void contextLoads() {
+		/**/
+	} // Fin de contextLoads().____________________________________________
+	
+	
 
-}
+} // FIN DE LA CLASSE ApplicationTests.--------------------------------------
