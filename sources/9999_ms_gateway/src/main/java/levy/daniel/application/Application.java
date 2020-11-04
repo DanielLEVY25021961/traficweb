d0a6141e78a254f239b978f8f50d397712e80c81
Daniel LEVY <dany.levy@free.fr>
2020-11-04 23:56:38 +0100

    GATEWAY-SERVICE : après modification des bootstrap.properties

--- sources/9999_ms_gateway/src/main/java/levy/daniel/application/Application.java
+++ sources/9999_ms_gateway/src/main/java/levy/daniel/application/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -16,12 +17,16 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * <li>L'annotation <code>SpringBootApplication</code> déclare à SPRING 
  * qu'il s'agit de la classe APPLICATIVE.</li>
  * <li>il s'agit d'un MICRO-SERVICE TECHNIQUE de <strong>GATEWAY 
- * (PROXY)</strong> point d'entrée unique des requêtes clientes.</li>
+ * (PROXY)</strong> point d'entrée unique des requêtes clientes 
+ * (pas d'annotation SPRING particulière).</li>
  * <li>L'annotation <code>EnableConfigurationProperties</code> déclare à SPRING 
  * que l'on peut écrire un Bean qui récupère les valeurs 
- * dans les applications.properties.</li>
+ * dans les <code>applications.properties</code>.</li>
  * <li>L'annotation <code>EnableDiscoveryClient</code> déclare à SPRING 
  * que ce Micro-Service s'enregistre auprès du serveur annuaire EUREKA.</li>
+ * <li>L'annotation <code>EnableHystrix</code> indique à SPRING que ce 
+ * Micro-Service est <strong>monitoré par le coupe-circuit 
+ * (circuit-breaker) HYSTRIX</strong></li>
  * <li>L'annotation <code>EnableSwagger2</code> déclare à SPRING 
  * que l'on va générer la documentation du MICRO-SERVICE avec Swagger2.</li>
  * </ul>
@@ -41,6 +46,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * @version 1.0
  * @since 4 nov. 2020
  */
+@EnableHystrix
 @EnableSwagger2
 @EnableDiscoveryClient
 @EnableConfigurationProperties
